dbdba744ffc3ea7aa56014ce7c346689e3f3b211
Willy Weiskopf <william@weiskopf.me>
2014-09-16 17:55:20 -0600

    .pcs files copied to the correct destination according to config.

--- loader.py
+++ loader.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+import shutil
 
 # for source, source_location in data['source'].items():
 #     if source in data:
@@ -9,9 +10,30 @@ import yaml
 #                     source = os.path.join(source_location, value)
 #                     destination = os.path.join(place, source, value)
 
-def load(self):
-    stream = open(self.config, 'r')
-    data = yaml.load(stream)
-    stream.close()
-    return data
+# Consider remake using [asdf] instead of .items()
 
+def init(self, config='config.yaml'):
+    stream = open(config, 'r')
+    self.data = yaml.load(stream)
+    stream.close()
+    return self.data
+
+def load(self):
+    for source_name, source in self.data['source'].items():
+        for fighter, textures in self.data['fighter'].items():
+                for number, source_path in textures.items():
+                    for name, destination in self.data['destination'].items():
+                        source_fullpath = os.path.join(
+                            source,
+                            source_path + '.pcs'
+                        )
+                        destination_fullpath = os.path.join(
+                            destination, 
+                            fighter,
+                            'Fit' + fighter + '{0:02d}'.format(number) + '.pcs'
+                        )
+                        os.makedirs(os.path.join(destination, fighter))
+                        shutil.copy2(source_fullpath,destination_fullpath)
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/loader_test.py
+++ tests/loader_test.py
@@ -1,21 +1,35 @@
 import unittest
 import os
+import shutil
+
 import loader
 
 os.chdir('tests')
 
 class TestTextureLoaderFunctions(unittest.TestCase):
     def setUp(self):
-        self.config = 'config.yaml'
-        self.data = loader.load(self)
+        self.data = loader.init(self)
+        loader.load(self)
 
-    def test_load(self):
+    def test_load_folders(self):
         """Source folders exist."""
         for source_name, source in self.data['source'].items():
             for fighter in self.data['fighter']:
                 for number, path in self.data['fighter'][fighter].items():
                     with self.subTest(path=path):
                         self.assertTrue(os.path.exists(os.path.join(source, fighter)))
+
+    def test_load_files(self):
+        """Source files exist."""
+        for source_name, source in self.data['source'].items():
+            for fighter, textures in self.data['fighter'].items():
+                for number, path in textures.items():
+                    fullpath = os.path.join(
+                        source, 
+                        path + '.pcs'
+                    )
+                    with self.subTest(fullpath):
+                        self.assertTrue(os.path.exists(fullpath))
 
     def test_mkdirs(self):
         """Appropriate destination folders were created."""
@@ -24,8 +38,23 @@ class TestTextureLoaderFunctions(unittest.TestCase):
                 with self.subTest(fighter=fighter):
                     self.assertTrue(os.path.exists(os.path.join(destination, fighter)))
 
+    def test_cpfiles(self):
+        """Files are copied from source to destination and renamed."""
+        for name, destination in self.data['destination'].items():
+            for fighter, textures in self.data['fighter'].items():
+                for number in textures:
+                    fullpath = os.path.join(
+                        destination, 
+                        fighter,
+                        'Fit' + fighter + '{0:02d}'.format(number) + '.pcs'
+                    )
+                    with self.subTest(fullpath):
+                        self.assertTrue(os.path.exists(fullpath))
+
     def tearDown(self):
-        pass
+        for name, destination in self.data['destination'].items():
+            shutil.rmtree(destination)
+            os.makedirs(destination)
 
 if __name__ == '__main__':
     unittest.main()
