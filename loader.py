061d788a36bcf5cfd925f21257dca1c799d5e80f
Willy Weiskopf <william@weiskopf.me>
2014-09-17 16:57:34 -0600

    Handle RESERVED keyword.

--- loader.py
+++ loader.py
@@ -2,6 +2,8 @@ import os
 import yaml
 import shutil
 
+RESERVED = 'RESERVED'
+
 def init(self, config='config.yaml'):
     stream = open(config, 'r')
     self.data = yaml.load(stream)
@@ -12,19 +14,19 @@ def load(self):
     for category, source in self.data['source'].items():
         for unit in self.data[category]:
             for number, path in self.data[category][unit].items():
-                for destination in self.data['destination'].values():
-                    source_fullpath = os.path.join(source, path)
-                    destination_fullpath = os.path.join(destination, unit, _filename_format(category, unit, number))
-                    if not os.path.exists(os.path.dirname(destination_fullpath)):
-                        os.makedirs(os.path.dirname(destination_fullpath))
-                    source_name, extension = os.path.splitext(path)
-                    extension = extension[1:]
-                    if extension is '':
-                        for filetype in self.data['filetype']:
-                            shutil.copy2('{0}.{1}'.format(source_fullpath, filetype)
-, '{0}.{1}'.format(destination_fullpath, filetype))
-                    else:
-                        shutil.copy2(source_fullpath, '{0}.{1}'.format(destination_fullpath, extension))
+                if path != RESERVED:
+                    for destination in self.data['destination'].values():
+                        source_fullpath = os.path.join(source, path)
+                        destination_fullpath = os.path.join(destination, unit, _filename_format(category, unit, number))
+                        if not os.path.exists(os.path.dirname(destination_fullpath)):
+                            os.makedirs(os.path.dirname(destination_fullpath))
+                        extension = os.path.splitext(path)[1]
+                        if extension is '':
+                            for filetype in self.data['filetype']:
+                                shutil.copy2('{0}.{1}'.format(source_fullpath, filetype)
+    , '{0}.{1}'.format(destination_fullpath, filetype))
+                        else:
+                            shutil.copy2(source_fullpath, destination_fullpath + extension)
 
 def _filename_format(category, *args):
     return {
--- tests/loader_test.py
+++ tests/loader_test.py
@@ -1,15 +1,17 @@
 import unittest
 import os
 import shutil
+import glob
 
 import loader
+
+# Tests should probably be highly specific cases and not use config file.
 
 os.chdir('tests')
 
 class TestTextureLoaderFunctions(unittest.TestCase):
     def setUp(self):
         self.data = loader.init(self)
-        loader.load(self)
 
     def test_load_folders(self):
         """Source folders exist."""
@@ -24,39 +26,51 @@ class TestTextureLoaderFunctions(unittest.TestCase):
         for source_name, source in self.data['source'].items():
             for fighter, textures in self.data['fighter'].items():
                 for number, path in self.data['fighter'][fighter].items():
-                    fullpath = os.path.join(source, path)
-                    source_name, extension = os.path.splitext(path)
-                    if extension is '':
-                        for filetype in self.data['filetype']:
-                            with self.subTest('{0}.{1}'.format(fullpath, filetype)):
-                                self.assertTrue(os.path.exists('{0}.{1}'.format(fullpath, filetype)))
-                    else:
-                        with self.subTest('{0}.{1}'.format(fullpath, path[:3])):
-                            self.assertTrue(os.path.exists(fullpath))
+                    if path != 'RESERVED':
+                        fullpath = os.path.join(source, path)
+                        extension = os.path.splitext(path)[1]
+                        if extension == '':
+                            for filetype in self.data['filetype']:
+                                with self.subTest('{0}.{1}'.format(fullpath, filetype)):
+                                    self.assertTrue(os.path.exists('{0}.{1}'.format(fullpath, filetype)))
+                        else:
+                            with self.subTest('{0}.{1}'.format(fullpath, path[:3])):
+                                self.assertTrue(os.path.exists(fullpath))
 
     def test_mkdirs(self):
         """Appropriate destination folders were created."""
+        loader.load(self)
         for name, destination in self.data['destination'].items():
             for fighter in self.data['fighter']:
-                with self.subTest(fighter=fighter):
-                    self.assertTrue(os.path.exists(os.path.join(destination, fighter)))
+                    with self.subTest(fighter=fighter):
+                        self.assertTrue(os.path.exists(os.path.join(destination, fighter)))
 
     def test_cpfiles(self):
         """Files are copied from source to destination and renamed."""
+        loader.load(self)
         for name, destination in self.data['destination'].items():
             for fighter in self.data['fighter']:
                 for number, path in self.data['fighter'][fighter].items():
-                    fullpath = os.path.join(destination, fighter, 'Fit{0}{1:02}'.format(fighter, number))
-                    source_name, extension = os.path.splitext(path)
-                    extension = extension[1:]
-                    if extension is '':
-                        for filetype in self.data['filetype']:
-                            with self.subTest('{0}.{1}'.format(fullpath, filetype)):
-                                self.assertTrue(os.path.exists('{0}.{1}'.format(fullpath, filetype)))
-                    else:
-                        with self.subTest('{0}.{1}'.format(fullpath, extension)):
-                            self.assertTrue(os.path.exists('{0}.{1}'.format(fullpath, extension)))
-                        
+                    if path != 'RESERVED':
+                        fullpath = os.path.join(destination, fighter, 'Fit{0}{1:02}'.format(fighter, number))
+                        source_name, extension = os.path.splitext(path)
+                        if extension == '':
+                            for filetype in self.data['filetype']:
+                                with self.subTest('{0}.{1}'.format(fullpath, filetype)):
+                                    self.assertTrue(os.path.exists('{0}.{1}'.format(fullpath, filetype)))
+                        else:
+                            with self.subTest(fullpath + extension):
+                                self.assertTrue(os.path.exists(fullpath + extension))
+
+    def test_reserved(self):
+        """Numbers marked as RESERVED are ignored."""
+        loader.load(self)
+        for name, destination in self.data['destination'].items():
+            for fighter in self.data['fighter']:
+                for number, path in self.data['fighter'][fighter].items():
+                    if path == 'RESERVED':
+                        with self.subTest('{0}, {1}'.format(fighter, number)):
+                            self.assertFalse(glob.glob('{0}/{1}/*{2:02d}*.*'.format(destination, fighter, number)))
 
     def tearDown(self):
         for name, destination in self.data['destination'].items():
