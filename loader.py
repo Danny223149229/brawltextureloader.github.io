6f7747105a923606fc6fc96caa53e35fab3621c3
Willy Weiskopf <william@weiskopf.me>
2014-09-16 20:21:25 -0600

    Rewrote to use [] a bit more than items(). Helper script for filename.

--- loader.py
+++ loader.py
@@ -10,8 +10,6 @@ import shutil
 #                     source = os.path.join(source_location, value)
 #                     destination = os.path.join(place, source, value)
 
-# Consider remake using [asdf] instead of .items()
-
 def init(self, config='config.yaml'):
     stream = open(config, 'r')
     self.data = yaml.load(stream)
@@ -19,21 +17,22 @@ def init(self, config='config.yaml'):
     return self.data
 
 def load(self):
-    for source_name, source in self.data['source'].items():
-        for fighter, textures in self.data['fighter'].items():
-                for number, source_path in textures.items():
-                    for name, destination in self.data['destination'].items():
-                        source_fullpath = os.path.join(
-                            source,
-                            source_path + '.pcs'
-                        )
-                        destination_fullpath = os.path.join(
-                            destination, 
-                            fighter,
-                            'Fit' + fighter + '{0:02d}'.format(number) + '.pcs'
-                        )
-                        os.makedirs(os.path.join(destination, fighter))
-                        shutil.copy2(source_fullpath,destination_fullpath)
+    for category, source in self.data['source'].items():
+        for unit in self.data[category]:
+            for number, path in self.data[category][unit].items():
+                for name, destination in self.data['destination'].items():
+                    source_fullpath = os.path.join(
+                        source,
+                        path + '.pcs'
+                    )
+                    destination_dirpath = os.path.join(
+                        destination, 
+                        unit
+                    )
+                    os.makedirs(destination_dirpath)
+                    shutil.copy2(source_fullpath, os.path.join(destination_dirpath, _filename_format(category, unit, number)))
 
-if __name__ == '__main__':
-    unittest.main()
+def _filename_format(category, *args):
+    return {
+        'fighter': 'Fit' + args[0] + '{0:02d}'.format(args[1]) + '.pcs',
+    }[category]
