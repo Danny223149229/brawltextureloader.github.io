f88bf3dbcccbd061244455818d84b5eaa533f473
Willy Weiskopf <william@weiskopf.me>
2014-09-16 20:23:39 -0600

    Removed items() from destination.

--- loader.py
+++ loader.py
@@ -2,14 +2,6 @@ import os
 import yaml
 import shutil
 
-# for source, source_location in data['source'].items():
-#     if source in data:
-#         for element, values in data[source].items():
-#             for key, value in values.items():
-#                 for place in data['destination']:
-#                     source = os.path.join(source_location, value)
-#                     destination = os.path.join(place, source, value)
-
 def init(self, config='config.yaml'):
     stream = open(config, 'r')
     self.data = yaml.load(stream)
@@ -20,7 +12,7 @@ def load(self):
     for category, source in self.data['source'].items():
         for unit in self.data[category]:
             for number, path in self.data[category][unit].items():
-                for name, destination in self.data['destination'].items():
+                for destination in self.data['destination'].values():
                     source_fullpath = os.path.join(
                         source,
                         path + '.pcs'
